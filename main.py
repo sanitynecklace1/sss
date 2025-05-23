98f92be431120bd2eee7cc96741d1179a9534e5a
sanitynecklace1 <SanityNecklace@yandex.com>
2025-05-23 18:40:50 +0300

    улучшение игрового цикла + движение ракеток + фикс спрайта мячика

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from pygame import *
 
 back = (167,3,12)
 win_width = 600
-wih_height = 500
-window = display.set_mode((win_width,wih_height))
+win_height = 500
+window = display.set_mode((win_width,win_height))
 window.fill(back)
 FPS = 60
+clock = time.Clock()
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
@@ -15,44 +16,48 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+    
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
 
 class Player(GameSprite):
     def update_r(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win.height == 80:
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
     def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win.height == 80:
+        if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
-
 
 game = True
 finish = False
-clock = time.Clock()
-fps = 60
 
 racket1 = Player('racket.png',30,200,4,50,150)
 racket2 = Player('racket.png',520,200,4,50,150)
-ball = GameSprite('tenis_ball.png', 200,200,4,50,150)
+ball = GameSprite('tenis_ball.png', 200,200,4,50,50)
 
 font.init()
 font = font.Font(None, 35)
 lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
 lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
-speed_x = 3
-speed_y = 3
-finish = False
-
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
     display.update()
     clock.tick(FPS)
